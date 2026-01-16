9a4e9d0bb8fb359bd2c7563abb648e02035389d3
JorgeMunozTaylor <munoz.taylor@hotmail.com>
2026-01-15 19:57:37 -0600

    Added button functionality

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,4 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit';
+import ProductList from './ProductList';
+
+
+
 
 export const CartSlice = createSlice({
   name: 'cart',
@@ -6,18 +10,51 @@ export const CartSlice = createSlice({
     items: [], // Initialize items as an empty array
   },
   reducers: {
+
     addItem: (state, action) => {
-    
+
+            const { name, image, cost } = action.payload;
+            const existingItem = state.items.find(item => item.name === name);
+
+            if (existingItem) {
+                // In existing items, quantity is already added as property
+                existingItem.quantity++;
+            } else {
+                state.items.push({ name, image, cost, quantity: 1 });
+            }
+
+            state.numOfItems += 1;
     },
+
+
     removeItem: (state, action) => {
+
+            const { name, quantity } = action.payload;
+            state.items = state.items.filter(item => item.name !== name);
+            state.numOfItems -= quantity;
+
+            // Just to be sure... I hate negative numbers
+            if (state.numOfItems < 0) {
+                state.numOfItems = 0;
+            }
     },
+
+
     updateQuantity: (state, action) => {
 
-    
+            const { name, quantity } = action.payload;
+            const existingItem = state.items.find(item => item.name === name);
+
+            if (existingItem) {
+                const differenceQuantity = quantity - existingItem.quantity;
+                state.numOfItems += differenceQuantity;
+                existingItem.quantity = quantity;
+            }
+     
     },
+
   },
 });
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
-
 export default CartSlice.reducer;
